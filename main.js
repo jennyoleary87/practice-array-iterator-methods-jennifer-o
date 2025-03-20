cd3ea9caa303db38474314b447fd3b366edcebf1
jennyo <jennifer.oleary.10@gmail.com>
2025-03-20 18:18:01 -0500

    complete tasks

--- main.js
+++ main.js
@@ -16,20 +16,40 @@ Task 1: Using forEach()
 3. Submit the output format by adding comments to the code.
 */
 
+let cities = ["Destin", "Duck", "Roach", "Ojai", "Orange Beach"];
+console.log("Favorite cities: ");
+cities.forEach((city) => {
+    console.log(city.toUpperCase());
+});
 
+/*
+Task 1 Output:
+DESTIN
+DUCK
+ROACH
+OJAI
+ORANGE BEACH
+*/
 
 /*
 Task 2: Transforming with map()
 1. Create an array called numbers with the numbers 1-5.
 2. Use map( ) to create an array where each number is squared. Call this array
 squares.
-*/
-
-
-
-/*
 3. Log the new array.
 4. Submit the output by adding comments to the code.
+*/
+
+let numbers = [1, 2, 3, 4, 5];
+let squares = numbers.map((num) => num ** 2);
+console.log("Numbers squared: ", squares);
+
+/*
+Task 2 Output:
+Numbers squared: [1, 4, 9, 16, 25]
+*/
+
+/*
 Task 3: Filtering with filter()
 1. Create an array called scores containing the numbers 85, 42, 90, 75, 30, and
 100.
@@ -39,7 +59,14 @@ equal to 80. Call this array highScores.
 4. Submit the output by adding comments to the code.
 */
 
+let scores = [85, 42, 90, 75, 30, 100];
+let highScores = scores.filter((score) => score >= 80);
+console.log("High scores: ", highScores);
 
+/*
+Task 3 Output:
+High scores: [85, 90, 100]
+*/
 
 /*
 Task 4: Finding with find() and findIndex()
@@ -47,4 +74,16 @@ Task 4: Finding with find() and findIndex()
 dishes. Try to add 5 or 6 elements.
 2. Use find() to locate the first food with more than 4 letters.
 3. Use findIndex( ) to find the index of that fruit.
+*/
+
+let favoriteFood = ["kiwi", "pie", "hibachi", "lasagna", "sushi", "salad"];
+let fourLetterFood = favoriteFood.find(food => food.length > 4);
+let fourLetterFoodIndex = favoriteFood.findIndex((food) => food.length > 4);
+console.log("First four letter food: ", fourLetterFood);
+console.log("Index of first four letter food: ", fourLetterFoodIndex);
+
+/*
+Task 4 Output:
+First four letter food: hibachi
+Index of first four letter food: 2
 */
